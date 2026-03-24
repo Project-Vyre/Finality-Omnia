ff792fc041d447454787b23bd4e096487bbcd023
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2026-03-24 02:02:15 -0400

    JS formatting...

--- kubejs/server_scripts/src/handlers/blakebr0/msyticalagriculture_core.js
+++ kubejs/server_scripts/src/handlers/blakebr0/msyticalagriculture_core.js
@@ -863,7 +863,7 @@ ServerEvents.recipes(event => {
   event.recipes.create.mixing('mysticalagriculture:supremium_ingot', [
     'mysticalagriculture:prosperity_ingot',
     '4x mysticalagriculture:supremium_essence',
-    {fluidTag: 'forge:molten_netherite', amount: 180}
+    { fluidTag: 'forge:molten_netherite', amount: 180 }
   ]).id('finality:mysticalagriculture/mixing/supremium_ingot')
 
   event.recipes.create.mixing('mysticalagriculture:supremium_gemstone', [
--- kubejs/server_scripts/src/handlers/tetra.js
+++ kubejs/server_scripts/src/handlers/tetra.js
@@ -10,32 +10,32 @@ ServerEvents.recipes(event => {
   event.recipes.create.mixing('tetra:forged_bolt', [
     '2x kubejs:iridium_nugget',
     'minecraft:deepslate',
-    {fluidTag: 'forge:molten_netherite', amount: 20}
+    { fluidTag: 'forge:molten_netherite', amount: 20 }
   ]).superheated().id('kubejs:tetra/mixing/bolt')
 
   event.recipes.create.mixing('tetra:forged_beam', [
     'kubejs:iridium_rod',
     'minecraft:deepslate',
-    {fluidTag: 'forge:molten_netherite', amount: 10}
+    { fluidTag: 'forge:molten_netherite', amount: 10 }
   ]).superheated().id('kubejs:tetra/mixing/forged_beam')
 
   event.recipes.create.compacting('tetra:forged_mesh', [
     'kubejs:iridium_sheet',
     'minecraft:deepslate',
-    {fluidTag: 'forge:molten_netherite', amount: 90}
+    { fluidTag: 'forge:molten_netherite', amount: 90 }
   ]).superheated().id('kubejs:tetra/mixing/forged_mesh')
 
   event.recipes.create.mixing('tetra:quick_latch', [
     'kubejs:iridium_nugget',
     'minecraft:tripwire_hook',
     'minecraft:deepslate',
-    {fluidTag: 'forge:molten_netherite', amount: 10}
+    { fluidTag: 'forge:molten_netherite', amount: 10 }
   ]).superheated().id('kubejs:tetra/mixing/quick_latch')
 
   event.recipes.create.mixing('tetra:vent_plate', [
     'kubejs:iridium_sheet',
     'kubejs:netherite_nugget',
     'minecraft:deepslate',
-    {fluidTag: 'forge:molten_netherite', amount: 90}
+    { fluidTag: 'forge:molten_netherite', amount: 90 }
   ]).superheated().id('kubejs:tetra/mixing/vent_plate')
 })
