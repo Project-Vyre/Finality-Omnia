d0e903cb838753eac8065657b72ae3cb8911469a
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2026-04-09 09:35:48 -0400

    Pre-emptively append `.noMirror()` to mechanical crafting recipes that should not be mirrored in breaking_bits.js.
    - My fault for assuming the recipes were not mirrored by default.

--- kubejs/server_scripts/src/core/recipes/create/breaking_bits.js
+++ kubejs/server_scripts/src/core/recipes/create/breaking_bits.js
@@ -56,7 +56,7 @@ ServerEvents.recipes(event => {
     ], {
       '0': 'kubejs:zero',
       '1': 'kubejs:one'
-    }).id(`finality:mechanical_crafting/${output}`)
+    }).noMirror().id(`kubejs:mechanical_crafting/${output}`)
   }
   let HEXCODES = (out, arrangement) => {
     event.recipes.create.mechanical_crafting(`kubejs:color_${out}`, [
@@ -65,14 +65,14 @@ ServerEvents.recipes(event => {
       '#': 'kubejs:octothorpe',
       '0': 'kubejs:zero',
       'F': 'kubejs:letter_f'
-    }).id(`finality:mechanical_crafting/color_${out}`)
+    }).noMirror().id(`kubejs:mechanical_crafting/color_${out}`)
   }
   let COLOR_MIXING = (output_color, color_one, color_two) => {
     event.recipes.create.mixing(`kubejs:color_${output_color}`, [
       `kubejs:color_${color_one}`,
       `kubejs:color_${color_two}`,
       Fluid.of('kubejs:shimmer', 500)
-    ]).id(`finality:mixing/color_${output_color}`)
+    ]).id(`kubejs:mixing/color_${output_color}`)
   }
   event.recipes.create.mechanical_crafting('create:handheld_worldshaper', [
     'CPULOO',
@@ -84,7 +84,7 @@ ServerEvents.recipes(event => {
     L: 'kubejs:command_block',
     O: 'kubejs:high_entropy_alloy_sheet',
     I: 'kubejs:high_entropy_alloy'
-  }).id('finality:mechanical_crafting/create_worldshaper')
+  }).id('kubejs:mechanical_crafting/create_worldshaper')
   for (let i = 0; i < CMD.length; i++) {
     let insert = CMD[i];
     event.recipes.create.deploying([
@@ -94,14 +94,14 @@ ServerEvents.recipes(event => {
     ], [
       `kubejs:${insert}`,
       'kubejs:final_pickaxe'
-    ]).keepHeldItem().id(`finality:binary_from_${insert}`)
+    ]).keepHeldItem().id(`kubejs:binary_from_${insert}`)
     event.recipes.create.deploying([
       'minecraft:structure_void',
       `kubejs:${insert}`
     ], [
       `kubejs:${insert}`,
       'kubejs:final_hoe'
-    ]).keepHeldItem().id(`finality:structure_void_from_${insert}`)
+    ]).keepHeldItem().id(`kubejs:structure_void_from_${insert}`)
   }
   event.recipes.create.deploying([
     'kubejs:command_block',
@@ -109,14 +109,14 @@ ServerEvents.recipes(event => {
   ], [
     'kubejs:command_block',
     'kubejs:final_axe'
-  ]).keepHeldItem().id('finality:uncolored_rectangle')
+  ]).keepHeldItem().id('kubejs:uncolored_rectangle')
   event.recipes.create.deploying([
     'kubejs:chain_command_block',
     'kubejs:uncolored_circle'
   ], [
     'kubejs:chain_command_block',
     'kubejs:final_axe'
-  ]).keepHeldItem().id('finality:uncolored_circle')
+  ]).keepHeldItem().id('kubejs:uncolored_circle')
   event.recipes.create.deploying([
     'kubejs:repeating_command_block',
     'kubejs:uncolored_star',
@@ -124,19 +124,19 @@ ServerEvents.recipes(event => {
   ], [
     'kubejs:repeating_command_block',
     'kubejs:final_axe'
-  ]).keepHeldItem().id('finality:uncolored_star')
+  ]).keepHeldItem().id('kubejs:uncolored_star')
   event.recipes.create.mechanical_crafting('kubejs:color_uncolored', [
     '#AAAAAA'
   ], {
     '#': 'kubejs:octothorpe',
     'A': 'kubejs:letter_a'
-  }).id('finality:mechanical_crafting/uncolored')
+  }).noMirror().id('kubejs:mechanical_crafting/uncolored')
   event.recipes.create.mechanical_crafting('kubejs:color_white', [
     '#FFFFFF'
   ], {
     '#': 'kubejs:octothorpe',
     'F': 'kubejs:letter_f'
-  }).id('finality:mechanical_crafting/color_white')
+  }).noMirror().id('kubejs:mechanical_crafting/color_white')
   HEXCODES('red', '#FF0000')
   HEXCODES('green', '#00FF00')
   HEXCODES('blue', '#0000FF')
@@ -151,7 +151,7 @@ ServerEvents.recipes(event => {
     'kubejs:color_green',
     'kubejs:color_blue',
     Fluid.of('kubejs:shimmer', 250)
-  ]).id('finality:mixing/color_white')
+  ]).id('kubejs:mixing/color_white')
   for (const [letter, binary] of Object.entries(LETTER_BINARY_CODES)) {
     BINARYCONVERSION(`${letter}`, `${binary}`)
   }
@@ -184,7 +184,7 @@ ServerEvents.recipes(event => {
     'I': 'kubejs:letter_i',
     'X': 'kubejs:letter_x',
     'Q': ['kubejs:denied_result', 'kubejs:removed_item', 'kubejs:errored_result']
-  }).id('finality:mechanical_crafting/qubit')
+  }).noMirror().id('kubejs:mechanical_crafting/qubit')
   // give
   event.recipes.create.mechanical_crafting('64x minecraft:netherite_block', [
     '/GIVE @S ',
@@ -209,7 +209,7 @@ ServerEvents.recipes(event => {
     'K': 'kubejs:letter_k',
     '6': 'kubejs:six',
     '4': 'kubejs:four'
-  }).id('finality:command/netherite_block_stack')
+  }).noMirror().id('kubejs:command/netherite_block_stack')
   event.recipes.create.mechanical_crafting('64x minecraft:diamond_block', [
     '/GIVE @S ',
     'DIAMOND  ',
@@ -233,8 +233,7 @@ ServerEvents.recipes(event => {
     'K': 'kubejs:letter_k',
     '6': 'kubejs:six',
     '4': 'kubejs:four'
-  }).id('finality:command/diamond_block_stack')
-  /*
+  }).noMirror().id('kubejs:command/diamond_block_stack')
   event.recipes.create.mechanical_crafting('64x create:blaze_cake', [
     '/GIVE @S ',
     'CREATE   ',
@@ -258,30 +257,29 @@ ServerEvents.recipes(event => {
     'K': 'kubejs:letter_k',
     '6': 'kubejs:six',
     '4': 'kubejs:four'
-  }).id('finality:command/blaze_cake_stack')
-  */
+  }).noMirror().id('kubejs:command/blaze_cake_stack')
   for (let [shape, string] of Object.entries(global.SHAPES)) {
     for (let color of VALID_COLOR_MIX) {
       event.recipes.create.mixing(`kubejs:${color}_${shape}`, [
         `kubejs:uncolored_${shape}`,
         `kubejs:color_${color}`,
         Fluid.of('create:honey', 250)
-      ]).heated().id(`finality:uncolored_to_${color}_${shape}_mixing`)
+      ]).heated().id(`kubejs:uncolored_to_${color}_${shape}_mixing`)
       event.recipes.create.mixing(`kubejs:uncolored_${shape}`, [
         `kubejs:${color}_${shape}`,
         'supplementaries:soap',
         Fluid.of('minecraft:water', 500)
-      ]).id(`finality:${color}_${shape}_washing`)
+      ]).id(`kubejs:${color}_${shape}_washing`)
     }
     for (let [color, string] of Object.entries(global.RGBWCMY)) {
       event.recipes.create.cutting([
         `kubejs:${color}_left_half_${shape}`,
         `kubejs:${color}_right_half_${shape}`
-      ], `kubejs:${color}_${shape}`).id(`finality:${color}_${shape}_halving`)
+      ], `kubejs:${color}_${shape}`).id(`kubejs:${color}_${shape}_halving`)
       event.recipes.create.cutting(`2x kubejs:${color}_${shape}_corner`, `kubejs:${color}_left_half_${shape}`)
-        .id(`finality:${color}_${shape}_left_half_halving`)
+        .id(`kubejs:${color}_${shape}_left_half_halving`)
       event.recipes.create.cutting(`2x kubejs:${color}_${shape}_corner`, `kubejs:${color}_right_half_${shape}`)
-        .id(`finality:${color}_${shape}_right_half_halving`)
+        .id(`kubejs:${color}_${shape}_right_half_halving`)
     }
   }
   event.recipes.create.sequenced_assembly('kubejs:star_of_light_shape', 'kubejs:red_star', [
@@ -298,14 +296,14 @@ ServerEvents.recipes(event => {
   ], {
     R: 'kubejs:blue_rectangle_corner',
     C: 'kubejs:blue_circle_corner'
-  }).id('finality:blueprint_shape_base')
-  event.recipes.create.deploying('kubejs:blueprint_shape', ['kubejs:blueprint_shape_base', 'kubejs:white_circle']).id('finality:blueprint_shape')
+  }).id('kubejs:blueprint_shape_base')
+  event.recipes.create.deploying('kubejs:blueprint_shape', ['kubejs:blueprint_shape_base', 'kubejs:white_circle']).id('kubejs:blueprint_shape')
   event.recipes.create.mechanical_crafting('kubejs:cpu_foundation', [
     ' R',
     'R '
   ], {
     R: 'kubejs:green_rectangle_corner'
-  }).id('finality:cpu_foundation')
+  }).id('kubejs:cpu_foundation')
   event.recipes.create.sequenced_assembly([
     Item.of('kubejs:cpu_substrate_shape').withChance(240.0),
     Item.of('kubejs:green_rectangle_corner', 2).withChance(70.0),
@@ -314,21 +312,21 @@ ServerEvents.recipes(event => {
   ], 'kubejs:cpu_foundation', [
     event.recipes.create.deploying('kubejs:incomplete_cpu_substrate_shape', ['kubejs:incomplete_cpu_substrate_shape', 'kubejs:white_circle_corner']),
     event.recipes.create.deploying('kubejs:incomplete_cpu_substrate_shape', ['kubejs:incomplete_cpu_substrate_shape', 'kubejs:white_circle_corner']),
-  ]).transitionalItem('kubejs:incomplete_cpu_substrate_shape').loops(1).id('finality:cpu_substrate_creation')
+  ]).transitionalItem('kubejs:incomplete_cpu_substrate_shape').loops(1).id('kubejs:cpu_substrate_creation')
   event.recipes.create.sequenced_assembly([
     'kubejs:cpu_shape'
   ], 'kubejs:cpu_substrate_shape', [
     event.recipes.create.deploying('kubejs:incomplete_cpu_shape', ['kubejs:incomplete_cpu_shape', 'kubejs:white_rectangle_corner']),
     event.recipes.create.deploying('kubejs:incomplete_cpu_shape', ['kubejs:incomplete_cpu_shape', 'kubejs:green_rectangle_corner'])
-  ]).transitionalItem('kubejs:incomplete_cpu_shape').loops(2).id('finality:cpu_shape')
+  ]).transitionalItem('kubejs:incomplete_cpu_shape').loops(2).id('kubejs:cpu_shape')
   event.recipes.create.mechanical_crafting('kubejs:emitter_shape_base', [
     ' C',
     'CR'
   ], {
     C: 'kubejs:magenta_circle_corner',
     R: 'kubejs:magenta_rectangle_corner'
-  }).id('finality:emitter_shape_base')
-  event.recipes.create.deploying('kubejs:emitter_shape', ['kubejs:emitter_shape_base', 'kubejs:white_star']).id('finality:emitter_shape')
+  }).id('kubejs:emitter_shape_base')
+  event.recipes.create.deploying('kubejs:emitter_shape', ['kubejs:emitter_shape_base', 'kubejs:white_star']).id('kubejs:emitter_shape')
   event.shaped('kubejs:null_storage_block', [
     'TTT',
     'TBT',
@@ -336,7 +334,7 @@ ServerEvents.recipes(event => {
   ], {
     T: 'minecraft:tinted_glass',
     B: 'minecraft:barrel'
-  }).id('finality:crafting/null_storage_block')
+  }).id('kubejs:crafting/null_storage_block')
   event.recipes.create.mixing([
     Item.of('kubejs:qubit').withChance(0.75),
     Item.of('kubejs:errored_result').withChance(0.25)
@@ -344,7 +342,7 @@ ServerEvents.recipes(event => {
     'kubejs:zero',
     'kubejs:one',
     'kubejs:chromatic_concrete_singularity'
-  ]).superheated().id('finality:mixing/qubit')
+  ]).superheated().id('kubejs:mixing/qubit')
   event.recipes.create.sequenced_assembly([
     Item.of('kubejs:zero').withChance(0.10),
     Item.of('kubejs:one').withChance(0.10),
@@ -361,5 +359,5 @@ ServerEvents.recipes(event => {
     event.recipes.create.deploying('kubejs:stabilizing_qubit', ['kubejs:stabilizing_qubit', 'minecraft:blue_ice']),
     event.recipes.create.deploying('kubejs:stabilizing_qubit', ['kubejs:stabilizing_qubit', 'minecraft:blue_ice']),
     event.recipes.create.deploying('kubejs:stabilizing_qubit', ['kubejs:stabilizing_qubit', 'minecraft:blue_ice'])
-  ]).transitionalItem('kubejs:stabilizing_qubit').loops(1).id('finality:sequenced_assembly/qubit_stabilization')
+  ]).transitionalItem('kubejs:stabilizing_qubit').loops(1).id('kubejs:sequenced_assembly/qubit_stabilization')
 })
