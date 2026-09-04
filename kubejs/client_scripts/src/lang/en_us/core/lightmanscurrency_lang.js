677cc854fb9392013cf6aad3fb04a3eb14246a90
CelestialAbyss <30322176+CelestialAbyss@users.noreply.github.com>
2026-09-04 14:27:08 -0400

    Fix https://github.com/Project-Vyre/Finality-Omnia/issues/14 but may need to check GitHub links because this issue shouldn't be directed to Mote...
    - Fixed mistake for iron_coin.plural translation key.

--- kubejs/client_scripts/src/lang/en_us/core/lightmanscurrency_lang.js
+++ kubejs/client_scripts/src/lang/en_us/core/lightmanscurrency_lang.js
@@ -5,7 +5,7 @@ ClientEvents.lang('en_us', event => {
     "item.kubejs.lightman.copper_coin.initial": "Cu",
     "item.kubejs.lightman.copper_coin.plural": "Copper Coins",
     "item.kubejs.lightman.iron_coin.initial": "Fe",
-    "item.kubejs.lightman.iron_coin.plural": "Copper Coins",
+    "item.kubejs.lightman.iron_coin.plural": "Iron Coins",
     "item.kubejs.lightman.gold_coin.initial": "Au",
     "item.kubejs.lightman.gold_coin.plural": "Gold Coins",
     "item.kubejs.lightman.netherite_coin.initial": "n",
